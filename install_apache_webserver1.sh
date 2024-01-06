128b53215012e176c79b88d2c1fd191eb512e24a
venugopalreddy1322 <venugopalreddy1322@gmail.com>
2024-01-07 00:01:45 +0100

    changes in install_apache_webserver file

--- install_apache_webserver1.sh
+++ install_apache_webserver1.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-echo ${ aws_instance.webserver1.public_ip }
+echo "<h1>Deployed via Terraform <h2>from $(hostname -f)</h2></h1>" | sudo tee /var/www/html/index.html
+# echo ${ aws_instance.webserver1.public_ip }
